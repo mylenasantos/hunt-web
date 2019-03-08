625314b0d266da0b39151d47e39414bd0f4f4046
Mylena Santos <myleena.santos@icloud.com>
2019-03-08 16:55:49 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from './components/Header/Index.js';
+import './styles.css';
+
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+
+);
+
+
+
+export default App;
