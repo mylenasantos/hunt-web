953d5e0c4d0d426f49c61f668fcead0ed27b85ca
Mylena Santos <myleena.santos@icloud.com>
2019-04-02 15:45:40 -0300

    inserindo API para conexão, com layout da pagina

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Index.js';
+import Main from './pages/main';
+//import api from ".services/api";
 import './styles.css';
 
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 
 );
